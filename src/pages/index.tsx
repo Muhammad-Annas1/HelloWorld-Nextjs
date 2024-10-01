b27d7af359e325e736ade5d9cf5fd913513aee1a
Muhammad Annas <ma2259050@gmail.com>
2024-10-01 17:54:36 +0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return(
+    <div> 
+      <h1 className="align-middle text-center p-10 text-3xl font-bold underline text text-blue-500">Hello World</h1>
+      </div>
+  )
+
+}
